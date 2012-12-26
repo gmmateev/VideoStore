217aadbcba352502ed9ba96c268d361a19740b66
Georgi Mateev <gmmateev92@abv.bg>
2012-12-26 22:34:57 +0200

    Added registration

---
+++ www/js/RestProviders/UsersProvider.js
@@ -0,0 +1,17 @@
+﻿var UsersProvider = function () { };
+
+UsersProvider.register = function (username, authCode, callback, errorCallback){
+    $.ajax({
+        url: "http://videostores.apphb.com/api/stores/register-user",
+        dataType: "json",
+        type: "POST",
+        data: { username: username, authCode: authCode },
+        success: function (data) {
+            callback();
+        },
+        error: function (data) {
+            var message = JSON.parse(data.responseText).Message;
+            errorCallback(message);
+        }
+    });
+};
--- www/js/ViewModels/UserBarViewModel.js
+++ www/js/ViewModels/UserBarViewModel.js
@@ -2,13 +2,32 @@
     /*Private variables*/
     var self = this;
 
+    /*Private methods*/
+    var registerCallback = function () {
+        self.isRegistered(true);
+        self.registerMessage("Registered");
+    };
+
+    var registerErrorCallback = function (message) {
+        self.isRegistered(false);
+        self.registerFailed(true);
+        self.registerMessage(message);
+    };
+
     /*Public variables*/
     self.isRegistered = ko.observable(false);
-
-    self.username = ko.observable("bai pesho");
+    self.registerFailed = ko.observable(false);
+    self.username = ko.observable();
+    self.registerMessage = ko.observable();
+    self.registerAttempted = ko.computed(function () {
+        return self.isRegistered || self.registerFailed;
+    }, self);
 
     /*Public methods*/
     self.register = function () {
-        self.isRegistered(true);
+        var pass = $("#userPassword").val();
+        var authentication = self.username() + pass;
+        var code = Sha1.hash(authentication);
+        UsersProvider.register(self.username, code, registerCallback, registerErrorCallback);
     }
 };
